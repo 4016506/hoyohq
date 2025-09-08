c6210e99a21c4138a6d8ab0d367f0b66dfe0de0b
Haoming Song <song4016@stanford.edu>
2025-09-08 16:33:30 -0700

    Initial website

---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+import './home.css'
+
+function Home() {
+
+  return (
+    <>
+      <div className="content">
+        <h1 className="title" style={{ color: 'white' }}>4016506</h1>
+      </div>
+    </>
+  )
+}
+
+export default Home
